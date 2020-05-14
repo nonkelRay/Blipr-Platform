a500c6d077b7d1eabc23fad38fb8bdc9d8f632f7
Sam Ghyselinck <samlinck@artcoresociety.com>
2020-05-14 14:34:58 +0200

    ordered stylesheets

--- resources/views/landingspage.blade.php
+++ resources/views/landingspage.blade.php
@@ -6,53 +6,14 @@
 
         <title>Blipr</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        {{-- <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet"> --}}
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-        </style>
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}" >
     </head>
-    <body>
+    <body class="frontpage">
         <div class="flex-center position-ref full-height">
-
             <div class="content">
                 <div class="title m-b-md">
                     Landingspage Blipr 🔴
