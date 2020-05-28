5467ad2cd2c1496e1ffc0588faf67ab46b6e0c15
Cesar Peeters <cesar.peeters@gmail.com>
2020-05-28 09:16:12 +0200

    add SEO to landingpage
    Google analytics, Hotjar, Google tag mananger, Facebook and Twitter cards,
    Meta tags and mailchimp pop-up

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -4,11 +4,88 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Blipr</title>
+
+        <link rel="icon" type="image/png" href="images/favicon/Favicon.png" media="screen">
+
+        <!-- SEO -->
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-153290871-3"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-153290871-3');
+        </script>
+
+        <!-- Hotjar Tracking Code for https://www.blipr.app -->
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:1830470,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
+
+        <!-- Google Tag Manager -->
+        <script>
+            (function(w,d,s,l,i){
+                w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WXH9P4Q');
+        </script>
+        <!-- End Google Tag Manager -->
+
+        <!-- FACEBOOK AND TWITTER CARD -->
+        <meta property="og:url"                content="https://www.blipr.app/" />
+        <meta property="og:type"               content="Landingpage" />
+        <meta property="og:title"              content="Blipr | Your moments, your memories" />
+        <meta property="og:description"        content="Blipr lets u fully experience music and reduce smartphone usage at concerts." />
+        <meta property="og:image"              content="https://csar.be/logo-blipr.jpg">
+        <meta property="fb:app_id"             content="930729077340126" />
+    
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name=”twitter:site” content="@blipr" /> 
+        <meta name=”twitter:title” content="Blipr | Your moments, your memories" /> 
+        <meta name=”twitter:description” content="Blipr lets u fully experience music and reduce smartphone usage at concerts." /> 
+        <meta name=”twitter:image” content="https://csar.be/logo-blipr.jpg" />
+    
+        <!-- META TAGS -->
+        <meta name="description" content="Blipr lets u fully experience music and reduce smartphone usage at concerts.">
+        <meta name="keywords" content="blipr">
+        <meta name="keywords" content="music">
+        <meta name="keywords" content="reduces">
+        <meta name="keywords" content="smartphones">
+        <meta name="keywords" content="feeling">
+        <meta name="keywords" content="experience">
+        <meta name="keywords" content="re-experience">
+        <meta name="keywords" content="mood">
+        <meta name="keywords" content="stimulation">
+        <meta name="keywords" content="memories">
+        <meta name="keywords" content="music">
+        <meta name="keywords" content="concert">
+        <meta name="keywords" content="festival">
+
         <!-- Styles -->
         <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}" >
         <script src="{{ asset('js/app.js')}}"></script>
+
     </head>
     <body class="frontpage">
+
+        <!-- Google Tag Manager (noscript) -->
+        <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WXH9P4Q"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        </noscript>
+        <!-- End Google Tag Manager (noscript) -->
+
         <div class="mobile-menu">
             <div class="container">
                 <nav>
@@ -180,5 +257,9 @@
                 </div>
             </div>
         </footer>
+
+        <!-- Mailchimp pop-up script -->
+        <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/c422f71116982fc0fc65ef74f/f710971e8abc49e2bea974fd5.js");</script>
+
     </body>
 </html>
