e9f7e63672b3a10c890410b0afa442f04051b89c
Cesar Peeters <cesar.peeters@gmail.com>
2020-06-07 20:23:49 +0200

    fix error message login

--- resources/views/user/auth/login.blade.php
+++ resources/views/user/auth/login.blade.php
@@ -8,18 +8,13 @@
         <form class="form login-form" method="POST" action="{{ route('login') }}">
             <h1>Login</h1>
             @csrf
-            <input id="email" type="email" placeholder="E-mail" class="form-control field @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
             @error('email')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
             @enderror
+            <input id="email" type="email" placeholder="E-mail" class="form-control field @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
             <input id="password" type="password" placeholder="Password" class="form-control field @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-            @error('password')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
             <div class="form-check">
                 <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                 <label class="form-check-label" for="remember">
